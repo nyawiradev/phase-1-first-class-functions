cc17cf1d8f9ad96e93adaadf5ac9bcc92605b026
nyawiradev <njaginjoyce@gmail.com>
2022-06-08 01:40:28 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+const receivesAFunction = (callbbckFunc) => {
+   return callbbckFunc();
+}
+const returnsANamedFunction = () => {
+   function AFunc() {
+     
+      // Your Code Here!!
+  }
+  return AFunc;
+}
+const returnsAnAnonymousFunction = () => {
+   return () => {};
+}
+   
+
+
